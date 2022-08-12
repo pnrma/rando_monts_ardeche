f157c2e489af0dbdec8c369f58d240baaa5d447f
Parc naturel régional des Monts d'Ardèche <administrateur@parc-monts-ardeche.fr>
2022-08-12 17:31:34 +0200

    android

---
+++ ios/App/rando/randoApp.swift
@@ -0,0 +1,17 @@
+//
+//  randoApp.swift
+//  rando
+//
+//  Created by Macintosh on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct randoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
